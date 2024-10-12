fa0928b41e78237a922c13c5cb0c0fb073ef0097
xia <22371216@buaa.edu.cn>
2024-10-12 23:30:08 +0800

    debug 解析UnaryFunc错误

--- src/frontend/parser/expression/unary/UnaryFuncParser.java
+++ src/frontend/parser/expression/unary/UnaryFuncParser.java
@@ -1,8 +1,10 @@
 package frontend.parser.expression.unary;
 
+import frontend.Error;
 import frontend.ErrorHandler;
 import frontend.lexer.Token;
 import frontend.lexer.TokenIterator;
+import frontend.parser.ParserErrors;
 import frontend.parser.terminal.Ident;
 import frontend.parser.terminal.IdentParser;
 
@@ -26,14 +28,31 @@ public class UnaryFuncParser {
         if (token.getType().equals(Token.Type.RPARENT)) {
             funcRParams = null;
             rParent = token;
-        } else {
+        } else if (isExp(token)) {
             iterator.traceBack(1);
             FuncRParamsParser funcRParamsParser = new FuncRParamsParser(iterator);
             funcRParams = funcRParamsParser.parseFuncRParams();
             ErrorHandler errorHandler = new ErrorHandler(iterator);
             rParent = errorHandler.handleErrorJ();
+        } else {
+            funcRParams = null;
+            iterator.traceBack(2);
+            token = iterator.getNextToken();
+            Error error = new Error(Error.Type.j, ")", token.getLine());
+            ParserErrors.addError(error);
+            rParent = null;
         }
         UnaryFunc unaryFunc = new UnaryFunc(ident, lParent, funcRParams, rParent);
         return unaryFunc;
+    }
+
+    private boolean isExp(Token token) {
+        return token.getType().equals(Token.Type.LPARENT) ||
+                token.getType().equals(Token.Type.IDENFR) ||
+                token.getType().equals(Token.Type.INTCON) ||
+                token.getType().equals(Token.Type.CHRCON) ||
+                token.getType().equals(Token.Type.PLUS) ||
+                token.getType().equals(Token.Type.MINU) ||
+                token.getType().equals(Token.Type.NOT);
     }
 }
